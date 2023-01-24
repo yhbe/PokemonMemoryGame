ba6f384bba225045bfed8a1c4941bf16f94c09bf
SHEF <Shefqetmush@gmail.com>
2023-01-23 20:58:50 -0500

    Header Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <h1>Hello</h1>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <div className="header--container">
+      <h1>Pokemon Memory Game</h1>
+      <div>
+        <h2>Current Score:</h2>
+        <h2>Best Score:</h2>
+      </div>
+    </div>
+  );
+}
